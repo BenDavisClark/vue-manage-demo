f39b4e8d9b73a4871db57e47d39b8657aff3142a
huguang <758801405@qq.com>
2018-06-26 09:50:45 +0800

    test

---
+++ src/views/test/dialog.vue
@@ -0,0 +1,14 @@
+<!--子组件-->
+<template>
+  <el-dialog :visible.sync="show"></el-dialog>
+</template>
+
+<script>
+  import {mapState} from 'vuex';
+  export default {
+      //扩展运算符
+    ...mapState({
+      show:state => state.dialog.show
+    })
+  }
+</script>
---
+++ src/views/test/parent.vue
@@ -0,0 +1,17 @@
+<!--父组件中引入子组件-->
+<template>
+    <div>
+      <!--<a href="javascript:;" @click="$store.commit('switch_dialog')">点击</a>-->
+      <a href="javascript:;" @click="$store.dispatch('switch_dialog')">点击</a>
+      <t-dialog ></t-dialog>
+    </div>
+</template>
+
+<script>
+  import dialog from './dialog.vue'
+  export default {
+    components:{
+      "t-dialog":dialog
+    }
+  }
+</script>
